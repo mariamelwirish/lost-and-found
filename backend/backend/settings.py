1fb0b7fba000b1b76de3e45141073b057901a480
Dani Jaffal <danijaffal@hotmail.com>
2025-09-22 19:19:22 +0300

    Enhance signup process with password validation and hints in frontend; update backend serializers and settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,8 +17,6 @@ import os
 import environ
 from pathlib import Path
 
-
-
 # load_dotenv()
 
 # # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,7 +57,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
-
 
 # Application definition
 
@@ -108,7 +105,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -118,8 +114,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -137,8 +131,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+    {
+        "NAME": "accounts.validators.ComplexPasswordValidator"
+    }
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -150,7 +146,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -175,7 +170,7 @@ _email_host = env("EMAIL_HOST", default=None)
 _email_port = env("EMAIL_PORT", default=None)  # keep as string to avoid int('') crash
 _email_user = env("EMAIL_HOST_USER", default=None)
 _email_pass = env("EMAIL_HOST_PASSWORD", default=None)
-_email_tls  = env("EMAIL_USE_TLS", default=None)
+_email_tls = env("EMAIL_USE_TLS", default=None)
 
 if _email_host:
     EMAIL_HOST = _email_host
