9dbb08595973cd3f6108173988e96f3cad2a1883
mariamelwirish <mariamelwirish@gmail.com>
2025-11-20 01:48:47 +0200

    attempting to fix images issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,6 +107,11 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
+USE_VERCEL_BLOB = env.bool("USE_VERCEL_BLOB", default=False)
+
+if USE_VERCEL_BLOB:
+    INSTALLED_APPS.append("storages")
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -216,6 +221,25 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+if USE_VERCEL_BLOB:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_ACCESS_KEY_ID = env("VERCEL_BLOB_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("VERCEL_BLOB_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("VERCEL_BLOB_BUCKET")
+    AWS_S3_ENDPOINT_URL = env("VERCEL_BLOB_ENDPOINT")
+    AWS_S3_REGION_NAME = env("VERCEL_BLOB_REGION", default="auto")
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_ADDRESSING_STYLE = "virtual"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    public_base = env("VERCEL_BLOB_PUBLIC_URL").rstrip("/")
+    if public_base.startswith("http"):
+        AWS_S3_CUSTOM_DOMAIN = public_base.split("://", 1)[1]
+    else:
+        AWS_S3_CUSTOM_DOMAIN = public_base
+    MEDIA_URL = f"{public_base}/"
 
 
 CORS_ALLOW_ALL_ORIGINS = True
